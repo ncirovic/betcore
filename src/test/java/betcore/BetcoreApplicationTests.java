a2738848f361456c36c8ce56be7c90b6f60defa6
Nemanja Cirovic <cirovicnemanja23@gmail.com>
2026-05-21 12:17:44 +0200

    chore: initial Spring Boot project setup

---
+++ src/test/java/betcore/BetcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package betcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
